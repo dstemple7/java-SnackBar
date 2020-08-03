196d26d307163bb47c0cf28a90fa70ae160a064e
David Temple <dstemple7@gmail.com>
2020-08-03 16:54:58 -0500

    stretch met

--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -4,8 +4,6 @@ public class Main
 {
 	private static void workWithData()
 	{
-		System.out.println("Time for some grub!");
-
 		// public Snack(String name, int quantity, double cost, int vendingMachineId)
 		Snack s1 = new Snack("Chips", 36, 1.75, 1);
 		Snack s2 = new Snack("Chocolate Bar", 36, 1.00, 1);
@@ -22,63 +20,98 @@ public class Main
 		Customer c1 = new Customer("Jane", 45.25);
 		Customer c2 = new Customer("Bob", 33.14);
 
-		// Customer 1 cash on hand 37.75
-		// Quantity of snack 4 is 21
-		// Customer 1 cash on hand 35.75
-		// Quanity of snack 3 is 29
-		// Customer 2 cash on hand 28.14
-		// Quantity of snack 4 is 19
-		// Customer 1 cash on hand 45.75
-		// Customer 1 cash on hand 44.75
-		// Quantity of snack 2 is 35
-		// Quantity of snack 3 is 41
-		// customer 2 cash on hand 22.14
-		// Quantity of snack 3 is 38
-
 		 // PROCESSING
 		 // Customer 1 (Jane) buys 3 of snack 4 (Soda).
 		c1.buySnacks(s4.getTotalCost(3));
 		s4.snacksBought(3);
 		 // Print Customer 1 (Jane) Cash on hand.
-		System.out.println(c1.getName() + " cash on hand " + c1.getCashOnHand());
+		System.out.println("\n" + c1.getName() + " cash on hand $" + c1.getCashOnHand());
 		 // Print quantity of snack 4 (Soda).
-		System.out.println("Quantity of snack 4 is " + s4.getQuantity());
+		System.out.println("Quantity of snack 4 is " + s4.getQuantity() + "\n");
 		 // Customer 1 (Jane) buys 1 of snack 3 (Pretzel).
 		c1.buySnacks(s3.getTotalCost(1));
 		s3.snacksBought(1);
 		 // Print Customer 1 (Jane) Cash on hand.
-		System.out.println(c1.getName() + " cash on hand " + c1.getCashOnHand());
+		System.out.println(c1.getName() + " cash on hand $" + c1.getCashOnHand());
 		 // Print quantity of snack 3 (Pretzel).
-		System.out.println("Quantity of snack 3 is " + s3.getQuantity());
+		System.out.println("Quantity of snack 3 is " + s3.getQuantity() + "\n");
 		 // Customer 2 (Bob) buys 2 of snack 4 (Soda).
 		c2.buySnacks(s4.getTotalCost(2));
 		s4.snacksBought(2);
 		 // Print Customer 2 (Bob) Cash on Hand.
-		System.out.println(c2.getName() + " cash on hand " + c2.getCashOnHand());
+		System.out.println(c2.getName() + " cash on hand $" + c2.getCashOnHand());
 		 // Print quantity of snack 4 (Soda).
-		System.out.println("Quantity of snack 4 is " + s4.getQuantity());
+		System.out.println("Quantity of snack 4 is " + s4.getQuantity() + "\n");
 		 // Customer 1 (Jane) finds $10.
 		c1.addCash(10);
 		 // Print Customer 1 (Jane) Cash on Hand.
-		System.out.println(c1.getName() + " cash on hand " + c1.getCashOnHand());
+		System.out.println(c1.getName() + " cash on hand $" + c1.getCashOnHand() + "\n");
 		 // Customer 1 (Jane) buys 1 of snack 2 (Chocolate Bar).
 		c1.buySnacks(s2.getTotalCost(1));
 		s2.snacksBought(1);
 		 // Print Customer 1 (Jane) Cash on Hand.
-		System.out.println(c1.getName() + " cash on hand " + c1.getCashOnHand());
+		System.out.println(c1.getName() + " cash on hand $" + c1.getCashOnHand());
 		 // Print quantity of snack 2 (Chocolate Bar).
-		System.out.println("Quantity of snack 2 is " + s2.getQuantity());
+		System.out.println("Quantity of snack 2 is " + s2.getQuantity() + "\n");
 		 // Add 12 more items to snack 3 (Pretzel).
 		s3.addQuantity(12);
 		 // Print quantity of snack 3 (Pretzel).
-		System.out.println("Quantity of snack 3 is " + s3.getQuantity());
+		System.out.println("Quantity of snack 3 is " + s3.getQuantity() + "\n");
 		 // Customer 2 (Bob) buys 3 of snack 3 (Pretzel).
 		c2.buySnacks(s3.getTotalCost(3));
 		s3.snacksBought(3);
 		 // Print Customer 2 (Bob) Cash on hand.
-		System.out.println(c2.getName() + " cash on hand " + c2.getCashOnHand());
+		System.out.println(c2.getName() + " cash on hand $" + c2.getCashOnHand());
 		 // Print quantity of snack 3 (Pretzel).
-		System.out.println("Quantity of snack 3 is " + s3.getQuantity());
+		System.out.println("Quantity of snack 3 is " + s3.getQuantity() + "\n");
+
+		// Stretch Goals
+		System.out.println("Stretch Goals\n");
+
+		// Snack: Chips
+		System.out.println("Snack: " + s1.getName());
+		// Vending Machine: Food
+		System.out.println("Vending Machine: " + v1.getName());
+		// Quantity: 36
+		System.out.println("Quantity: " + s1.getQuantity());
+		// Total Cost: $63.00
+		System.out.println("Total Cost: $" + s1.getQuantity() * s1.getCost() + "0\n");
+
+		// Snack: Chocolate Bar
+		System.out.println("Snack: " + s2.getName());
+		// Vending Machine: Food
+		System.out.println("Vending Machine: " + v1.getName());
+		// Quantity: 35
+		System.out.println("Quantity: " + s2.getQuantity());
+		// Total Cost: $35.00
+		System.out.println("Total Cost: $" + s2.getQuantity() * s2.getCost() + "0\n");
+
+		// Snack: Pretzel
+		System.out.println("Snack: " + s3.getName());
+		// Vending Machine: Food
+		System.out.println("Vending Machine: " + v1.getName());
+		// Quantity: 38
+		System.out.println("Quantity: " + s3.getQuantity());
+		// Total Cost: $76.00
+		System.out.println("Total Cost: $" + s3.getQuantity() * s3.getCost() + "0\n");
+
+		// Snack: Soda
+		System.out.println("Snack: " + s4.getName());
+		// Vending Machine: Drink
+		System.out.println("Vending Machine: " + v2.getName());
+		// Quantity: 19
+		System.out.println("Quantity: " + s4.getQuantity());
+		// Total Cost: $47.50
+		System.out.println("Total Cost: $" + s4.getQuantity() * s4.getCost() + "0\n");
+
+		// Snack: Water
+		System.out.println("Snack: " + s5.getName());
+		// Vending Machine: Drink
+		System.out.println("Vending Machine: " + v2.getName());
+		// Quantity: 20
+		System.out.println("Quantity: " + s5.getQuantity());
+		// Total Cost: $55.00
+		System.out.println("Total Cost: $" + s5.getQuantity() * s5.getCost() + "0");
 	}
 
 	public static void main(String[] args)
